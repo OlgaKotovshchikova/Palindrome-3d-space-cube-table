7af302a649c8f96782660fc8cb6a00649e710759
Olga <olchiknega@mail.ru>
2022-08-16 22:18:26 +0700

    initial commit

---
+++ Palindrome-3d-space-cube-table/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Palindrome_3d_space_cube_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
